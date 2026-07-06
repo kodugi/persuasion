ba728cc05ec6c979d9da0b74f0c288b9c94e7eb9
opal2718 <160561732+opal2718@users.noreply.github.com>
2026-07-06 19:51:01 +0900

    Map auto generation logic

--- Assets/Scripts/Investigation/Inv_GameManager.cs
+++ Assets/Scripts/Investigation/Inv_GameManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 namespace Investigation
@@ -7,6 +9,51 @@ namespace Investigation
     public class Inv_GameManager : MonoBehaviour
     {
         private Dictionary<string, List<string>> notes = new Dictionary<string, List<string>>();
+        public GameObject notePanel;
+        public GameObject noteTitlePrefab;
+        public GameObject noteContentPrefab;
+        public GameObject interactablePrefab;
+        private void Awake()
+        {
+            //LoadDataFromFile();
+            
+        }
+        private void Start()
+        {
+            GameObject.FindFirstObjectByType<Canvas>().transform.Find("NoteButton").GetComponent<Button>().onClick.AddListener(ViewNotes);
+            SetScene();
+        }
+        private class Position
+        {
+            public float x;
+            public float y;
+        }
+        private class InteractableObj
+        {
+            public string title;
+            public Position Position;
+            public string image;
+        }
+        void SetScene()
+        {
+            string currScene = getID();
+            string path = "Assets/Scripts/Investigation/Dialogue/Maps/" + currScene + ".json";
+            string json = System.IO.File.ReadAllText(path);
+            JObject data = JObject.Parse(json);
+            List<InteractableObj> objects = JsonConvert.DeserializeObject<List<InteractableObj>>(data["interactables"].ToString());
+            foreach(InteractableObj obj in objects)
+            {
+                GameObject interactableParent = GameObject.Find("Interactables");
+                Vector2 position = new Vector2(obj.Position.x, obj.Position.y);
+                GameObject interactable = Instantiate(interactablePrefab,position, Quaternion.identity, interactableParent.transform);
+                interactable.name = obj.title;
+                //interactable.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(obj.image);
+            }
+        }
+        string getID()
+        {
+            return "Map1";
+        }
         public void AddNote(string noteName, List<string> contents)
         {
             if (!notes.ContainsKey(noteName))
@@ -18,36 +65,6 @@ namespace Investigation
                 notes[noteName].AddRange(contents);
             }
         }
-        private void Awake()
-        {
-            //LoadDataFromFile();
-        }
-        private void LoadDataFromFile()
-        {
-            string path = "Assets/notes.json";
-            if (System.IO.File.Exists(path))
-            {
-                string json = System.IO.File.ReadAllText(path);
-                notes = JsonUtility.FromJson<Dictionary<string, List<string>>>(json);
-            }
-        }
-        private void OnApplicationQuit()
-        {
-            //SaveDataToFile();
-        }
-        private void SaveDataToFile()
-        {
-            string path = "Assets/notes.json";
-            string json = JsonUtility.ToJson(notes, true);
-            System.IO.File.WriteAllText(path, json);
-        }
-        private void Start()
-        {
-            GameObject.FindFirstObjectByType<Canvas>().transform.Find("NoteButton").GetComponent<Button>().onClick.AddListener(ViewNotes);
-        }
-        public GameObject notePanel;
-        public GameObject noteTitlePrefab;
-        public GameObject noteContentPrefab;
         public void ViewNotes()
         {
             notePanel.SetActive(true);
@@ -70,6 +87,25 @@ namespace Investigation
                     noteContentItem.GetComponent<TMPro.TextMeshProUGUI>().text = this_content;
                 }
             }
+        }
+        private void LoadDataFromFile()
+        {
+            string path = "Assets/notes.json";
+            if (System.IO.File.Exists(path))
+            {
+                string json = System.IO.File.ReadAllText(path);
+                notes = JsonUtility.FromJson<Dictionary<string, List<string>>>(json);
+            }
+        }
+        private void OnApplicationQuit()
+        {
+            //SaveDataToFile();
+        }
+        private void SaveDataToFile()
+        {
+            string path = "Assets/notes.json";
+            string json = JsonUtility.ToJson(notes, true);
+            System.IO.File.WriteAllText(path, json);
         }
     }
 }
