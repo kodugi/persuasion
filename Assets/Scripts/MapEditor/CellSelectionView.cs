6883635a1bf653a2aeaca91b78b1a4d7c66a081c
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-07-06 17:30:21 +0900

    에디터 scene 추가 및 Newtonsoft.Json 설치

---
+++ Assets/Scripts/MapEditor/CellSelectionView.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using GamePlay;
+
+namespace MapEditor
+{
+    public class CellSelectionView : MonoBehaviour
+    {
+        [SerializeField] private Type[] _cellTypes;
+    
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
