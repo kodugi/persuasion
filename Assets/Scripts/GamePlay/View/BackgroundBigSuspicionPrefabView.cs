8749df7954b7685545e5ffc33c6717cd55a523fa
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-08-23 10:13:39 +0900

    BackgroundBigSuspicionView 표시 순서 변경

--- Assets/Scripts/GamePlay/View/BackgroundBigSuspicionPrefabView.cs
+++ Assets/Scripts/GamePlay/View/BackgroundBigSuspicionPrefabView.cs
@@ -1,11 +1,35 @@
+using UnityEngine;
+
 namespace GamePlay
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class BackgroundBigSuspicionPrefabView : SuspicionPrefabView
     {
+        private const int MarkerOccludingSortingOrder = 4;
+
+        private void Awake()
+        {
+            ApplyMarkerOccludingSorting();
+        }
+
+        private void OnValidate()
+        {
+            ApplyMarkerOccludingSorting();
+        }
+
         public void PlayBlinkAnimation()
         {
             // TODO: implement actual animation
             PlayPreGameOverAnimation();
+        }
+
+        private void ApplyMarkerOccludingSorting()
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.sortingOrder = MarkerOccludingSortingOrder;
+            }
         }
     }
 }
